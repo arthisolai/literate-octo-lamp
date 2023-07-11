f5effb7e3b60c0e7d82e4914b5624c581b3e708c
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-11 18:05:56 +0200

    upsi i forgot to push

---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,36 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+const myTitle = "Trip to Greece";
+const myText = "A bucketlist checked. It was a destination paradise";
+let noOfLikes = 66;
+const postCreatedBy = "Arthi";
+const isReported = false;
+console.log("Title : ", myTitle);
+console.log("Text : ", myText);
+console.log("No. of likes:  : ", noOfLikes);
+console.log("Created by : ", postCreatedBy);
+console.log("Is it reported : ", isReported);
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+const increasedLikes = noOfLikes++;
+console.log("Incresed no. of likes:  : ", noOfLikes);

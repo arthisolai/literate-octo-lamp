6d9f894d205356363507e801075e9e4cf47d80d7
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-13 14:55:06 +0200

    initial commit

--- js-elements/js-createelement_social-media-post/js/index.js
+++ js-elements/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,25 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const hTag = document.createElement("h1");
+hTag.classList.add("h1");
+hTag.textContent = "Social Media Post";
+document.querySelector("body").append(hTag);
+
+const articleTag = document.createElement("article");
+articleTag.classList.add("post");
+const pTag = document.createElement("p");
+pTag.classList.add("post__content");
+pTag.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+const footerTag = document.createElement("footer");
+footerTag.classList.add("post__footer");
+const spanTag = document.createElement("span");
+spanTag.classList.add("post__username");
+const buttonTag = document.createElement("button");
+buttonTag.classList.add("post__button");
+
+// buttonTag.append(footerTag);
+// spanTag.append(footerTag);
+// footerTag.append(articleTag);
+// pTag.append(articleTag);

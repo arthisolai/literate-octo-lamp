4c03f2cf43a793f741cf2b77b867d088af943cbd
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-17 17:11:37 +0200

    challenges done except bonus

--- JSLoops/js-loops_while-random-number/index.js
+++ JSLoops/js-loops_while-random-number/index.js
@@ -9,6 +9,12 @@ let count = 0;
 
 // --^-- write/change code here --^--
 
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random();
+}
+
 console.log(
   `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
 );

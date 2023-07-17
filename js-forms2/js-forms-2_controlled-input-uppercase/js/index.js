281b39da777148ce703ed9759e9f6fbf17d6004d
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-17 15:16:06 +0200

    Initial commit

--- js-forms2/js-forms-2_controlled-input-uppercase/js/index.js
+++ js-forms2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,7 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", function (event) {
+  event.target.value = event.target.value.toUpperCase();
+});
--- js-forms2/js-forms-2_simple-live-calculator/js/index.js
+++ js-forms2/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,21 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+let firstValue = 0;
+let secondValue = 0;
+
+firstInput.addEventListener("input", (event) => {
+  firstValue = event.target.value;
+  multiply(firstValue, secondValue);
+});
+
+secondInput.addEventListener("input", (event) => {
+  secondValue = event.target.value;
+  multiply(firstValue, secondValue);
+});
+
+function multiply(num1, num2) {
+  result.innerHTML = num1 * num2;
+  return;
+}

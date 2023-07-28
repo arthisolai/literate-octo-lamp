b206b43dfe012cd0746f028d5e8ec3aa4e006339
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-28 11:15:32 +0200

    3rd done

--- React-Basics/react-basics_attributes/src/App.js
+++ React-Basics/react-basics_attributes/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return (
+    <article className="article">
+      <h2 className="article__link">Know about Dogs</h2>
+      <label htmlFor="Search here"></label>
+      <input id="Search"></input>
+      <a className="article__link" href="https://en.wikipedia.org/wiki/Dog">
+        Article on Dog
+      </a>
+    </article>
+  );
 }

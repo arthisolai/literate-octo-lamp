32913056c575e19322b03de52e62b35814b13e6f
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-07-28 10:23:34 +0200

    init commit

--- JS-UnitTesting/js-unit-testing_people/index.js
+++ JS-UnitTesting/js-unit-testing_people/index.js
@@ -2,18 +2,46 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => person.firstName + " " + person.lastName);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => {
+    return person.age === age;
+  });
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => {
+      return person.age > age;
+    })
+    .map((person) => person.firstName + " " + person.lastName);
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  return people
+    .filter((person) => {
+      return person.lastName === lastName;
+    })
+    .map((person) => person.firstName + " " + person.lastName);
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  return people.find((people) => people.id === id);
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  return people.some((people) => people.age > age);
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  return people
+    .sort((a, b) => a.age - b.age)
+    .map((people) => `${people.firstName} ${people.lastName}`);
+}

7871ae9fe8bec95841c1895860480d0f742f8bec
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-02 12:04:19 +0200

    3rd done

--- React-State2/react-state-2_favourite-holiday/src/App.js
+++ React-State2/react-state-2_favourite-holiday/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [holiday, setHoliday] = useState("?");
+  const [date, setDate] = useState("?");
+
   function handleSubmit(event) {
     event.preventDefault();
+    const form = event.target;
+    setHoliday(form.elements.holiday.value);
+    setDate(form.elements.date.value);
   }
 
   return (
@@ -30,10 +36,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );

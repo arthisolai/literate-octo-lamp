2ca623aa0384b5e629b18d1e477c5b07c1d22d95
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-11 14:47:30 +0200

    init commit

--- React-GlobalState/react-global-state_lights/components/Light/index.js
+++ React-GlobalState/react-global-state_lights/components/Light/index.js
@@ -2,9 +2,7 @@ import { useState } from "react";
 import { LightButton, Icon, Text, Name, State } from "./Light.styled";
 
 export default function Light({ name, isOn, onToggle }) {
-  function handleToggle() {
-    setIsOn((isOn) => !isOn);
-  }
+  
 
   return (
     <LightButton type="button" onClick={onToggle} isOn={isOn}>

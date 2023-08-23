7125c4ae202fd693d301d07bbd4ec00c1aa5d5cd
arthisolai <137558405+arthisolai@users.noreply.github.com>
2023-08-23 11:19:36 +0200

    Backend create challenge done

--- Backend-Create/backend-create_products/pages/api/products/index.js
+++ Backend-Create/backend-create_products/pages/api/products/index.js
@@ -8,4 +8,17 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+  if (request.method === "POST") {
+    try {
+      const productsData = request.body;
+      const products = new Product(productsData);
+      await products.save();
+      response.status(201).json({ status: "Product created" });
+      //------------alternate way
+      // await Joke.create(request.body);
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
 }
